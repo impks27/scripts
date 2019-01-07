7f5845af3675bf81ec42f8ee4f149e3ce284c34b
impks27 <raj.ratedr001@gmail.com>
2019-01-07 17:26:31 +0000

    debug shell

--- dollar.sh
+++ dollar.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 echo $$
 echo $!
 echo $#
@@ -6,3 +6,6 @@ echo $*
 echo $@
 
 # $@ treats each quoted arguments as separate arguments but $* will consider the entire set of positional parameters as a single string.
+
+# $$ gives the process id of the currently executing process whereas; 
+# $! Shows the process id of the process that recently went into the background.
